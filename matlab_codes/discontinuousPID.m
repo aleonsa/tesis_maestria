fb3dcc9c9fceac6048fd82832bf20a3701f559bf
Alejandro Leon <aleons.jo@gmail.com>
2025-08-20 23:36:15 -0600

    20-aug

--- matlab_codes/discontinuousPID.m
+++ matlab_codes/discontinuousPID.m
@@ -6,18 +6,20 @@ theta1 = 3.0;
 theta2 = 1.0;   
 
 % Parámetros de la simulación
-Ts = 0.01;      
-Tfinal = 20;    
+Ts = 3e-5;      
+Tfinal = 30;    
 t = 0:Ts:Tfinal; 
 
 % CI
-x1 = 0;         
-x2 = 0;         
+x1_0 = 0.5;         
+x2_0 = 0.5;         
 
 % Parámetros de la señal senoidal de ref    
 w = 1;  
-ref = sin(w * t); % ref
-ref_dot = w*cos(w*t); % derivada de ref
+y0 = 1;
+ref = y0*sin(w * t); % referencia
+ref_dot = y0*w*cos(w*t);
+ref_ddot = -y0*(w^2)*sin(w*t);
 
 %% Controlador PID discontinuo
 
@@ -25,46 +27,39 @@ k1 = 23.4;
 k2 = 31.6;
 k3 = 20;
 
-% Variables para el controlador PID-disc
-e1 = 0;
-e2 = 0;
+% Condiciones iniciales en coordenadas de error
+e1 = x1_0 - ref(1);
+e2 = x2_0 - ref_dot(1);
+
 sigma = 0;
 
 %% Bucle de simulación
 
 % Vectores para guardar los resultados
-x1_hist = zeros(size(t));
-x2_hist = zeros(size(t));
+e1_hist = zeros(size(t));
+e2_hist = zeros(size(t));
 u_hist = zeros(size(t));
-e_hist = zeros(size(t));
 
-for i = 1:length(t)
-    
-    % --- calcular el error ---
-    e1 = x1 - ref(i);
-    e2 = x2 - ref_dot(i);
-
+% Loop de simulacion
+for i = 1:length(t)    
     % -- variable deslizante ---
     sigma_dot = sign(e1);
     
     % --- calcular la acción de control ---
-    u = PID_disc(e1,e2,sigma,k1,k2,k3);
+    u = PID_disc(e1,e2,sigma,k1,k2,k3); u = -u;
 
-    error_anterior = e1;
-    
-    % --- calcular las derivadas de los estados ---
-    x1dot, x2dot = SimulateDynamics(theta1,theta2,u);
+    e1dot = e2;
+    e2dot = -theta1 * sin(e1 + ref(i)) - ref_ddot(i)  + theta2*u;
 
     % --- integrar para obtener los nuevos estados  ---
-    x1 = x1 + x1dot * Ts;
-    x2 = x2 + x2dot * Ts;
+    e1 = e1 + e1dot * Ts;
+    e2 = e2 + e2dot * Ts;
     sigma = sigma + sigma_dot*Ts;
     
     % --- Paso 4.5: Guardar los valores actuales para graficar  ---
-    x1_hist(i) = x1;
-    x2_hist(i) = x2;
+    e1_hist(i) = e1;
+    e2_hist(i) = e2;
     u_hist(i) = u;
-    e_hist(i) = e1;
     
 end
 
@@ -73,13 +68,11 @@ figure;
 
 % Gráfica del seguimiento de la trayectoria
 subplot(2,1,1);
-plot(t, x1_hist, 'b', 'LineWidth', 2);
-hold on;
-plot(t, ref, 'r--', 'LineWidth', 1.5); % Graficamos la señal senoidal
-title('Seguimiento de Trayectoria Senoidal');
+plot(t, e1_hist, 'b', 'LineWidth', 2);
+title('Estado e_1 en el tiempo');
 xlabel('Tiempo (s)');
-ylabel('x1');
-legend('Respuesta del sistema', 'Referencia Senoidal');
+ylabel('e1');
+legend('Error de seguimiento');
 grid on;
 
 % Gráfica de la señal de control
@@ -89,3 +82,5 @@ title('Señal de Control (u)');
 xlabel('Tiempo (s)');
 ylabel('Amplitud de u');
 grid on;
+
+input('Press enter to close');
